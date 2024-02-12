068a20fafdd28c846e111804a28db80f91d5ab79
des267 <dstular@yahoo.com>
2024-02-12 12:43:48 -0700

    Initial commit

---
+++ temperatureController.c
@@ -0,0 +1,36 @@
+/**
+* temperatureController.c
+ *
+ * Author: Desmond Stular
+ *   Date: February 12, 2024
+*/
+
+#include "temperatureController.h"
+
+#define MS (48000000 / (8 * 1000))
+
+void setupLED() {
+
+}
+
+void setupTimer() {
+
+}
+
+void setupUART() {
+
+}
+
+void timerInterrupt() {
+
+}
+
+void uartInterrupt() {
+
+}
+
+void setupAll() {
+    setupLED();
+    setupUART();
+    setupTimer();
+}
---
+++ temperatureController.h
@@ -0,0 +1,12 @@
+/**
+* temperatureController.h
+ *
+ * Author: Desmond Stular
+ *   Date: February 12, 2024
+*/
+
+#ifndef TEMPERATURECONTROLLER_H_
+#define TEMPERATURECONTROLLER_H_
+
+
+#endif
